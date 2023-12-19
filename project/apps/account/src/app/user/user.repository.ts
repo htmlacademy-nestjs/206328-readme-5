4396486b4e925d4a2171559214b6619f9b3412d9
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-19 16:15:46 +0300

    fix(module2-task1): added injectable

--- project/apps/account/src/app/user/user.repository.ts
+++ project/apps/account/src/app/user/user.repository.ts
@@ -1,4 +1,6 @@
 import { BaseMemoryRepository } from "@project/shared/core";
 import { UserEntity } from "./user.entity";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class UserRepository extends BaseMemoryRepository<UserEntity> {}
