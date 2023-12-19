6687c45771451bc43df5a1fce4736a5421e9060d
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-19 08:31:25 +0300

    fix(module2-task1): added modules

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-
+import { UserModule } from './user/user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
