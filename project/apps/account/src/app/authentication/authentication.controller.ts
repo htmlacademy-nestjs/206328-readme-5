94753715bd74431f10b4ba8690b919aec1095b6a
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-23 12:18:08 +0300

    fix(module2-task1): added rgister

--- project/apps/account/src/app/authentication/authentication.controller.ts
+++ project/apps/account/src/app/authentication/authentication.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Post, Controller } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { fillDto } from '@project/shared/helpers';
+import { UserRdo } from './rdo/user.rdo';
 
 @Controller('authentication')
 export class AuthenticationController {
     constructor(private readonly authServis: AuthenticationService) {}
+
+    @Post('register')
+    public async create(@Body() dto: CreateUserDto) {
+        const newUser = await this.authServis.register(dto);
+        return fillDto(UserRdo, newUser.toPOJO());
+    }
 }
