7bcb1500dc90c80c2d6136f0feda01ab874e3eee
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-24 17:15:23 +0300

    fix(module2-task1): added media module

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-
+import { MediaModule } from './media/media.module';
 
 @Module({
-  imports: [],
+  imports: [MediaModule],
   controllers: [],
   providers: [],
 })
