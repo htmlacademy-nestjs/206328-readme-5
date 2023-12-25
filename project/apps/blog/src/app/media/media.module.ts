73f01437572e846df23921177d6731fb05039b4c
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-25 08:23:02 +0300

    fix(module2-task1): added service and controller

--- project/apps/blog/src/app/media/media.module.ts
+++ project/apps/blog/src/app/media/media.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MediaController } from './media.controller';
+import { MediaService } from './media.service';
 
-@Module({})
+@Module({
+  controllers: [MediaController],
+  providers: [MediaService],
+})
 export class MediaModule {}
