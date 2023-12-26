4b3148fbf3a80039e0929f1ec320a8aca354454a
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-26 03:52:55 +0300

    fix(module2-task1): added swagger

--- project/apps/blog/src/app/post/post.controller.ts
+++ project/apps/blog/src/app/post/post.controller.ts
@@ -1,20 +1,31 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, HttpStatus } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PostService } from './post.service';
 import { fillDto } from '@project/shared/helpers';
 import { CreatePostDto } from './dto/create-post-dto';
 import { PostRdo } from './rdo/post.rdo';
 import { PostEntity } from './post.entity';
 
+@ApiTags('blog')
 @Controller('post')
 export class PostController {
     constructor(private readonly postService: PostService) {}
 
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: 'The new post was successfully created.'
+    })
     @Post('create')
     public async create(@Body() dto: CreatePostDto) {
         const newPost = await this.postService.createPost(dto);
         return fillDto(PostRdo, newPost.toPOJO());
     }
 
+    @ApiResponse({
+        type: PostRdo,
+        status: HttpStatus.OK,
+        description: 'Post found.'
+    })
     @Get(':id')
     public async getPost(@Param('id') id: string) {
         const existPost = await this.postService.getPost({ id }) as PostEntity;
--- project/apps/blog/src/app/post/rdo/post.rdo.ts
+++ project/apps/blog/src/app/post/rdo/post.rdo.ts
@@ -1,21 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class PostRdo {
+    @ApiProperty({
+        description: 'post unique id',
+        example: 'kj34j345j-k3k4k53-k34k5ks'
+    })
     @Expose()
     public id: string;
 
+    @ApiProperty({
+        description: 'text of the post',
+        example: 'kj34j345j-k3k4k53-k34k5ks'
+    })
     @Expose()
     public text: string;
 
+    @ApiProperty({
+        description: 'author of the post',
+        example: 'kj34j345j-k3k4k53-k34k5ks'
+    })
     @Expose()
     public author: string;
 
+    @ApiProperty({
+        description: 'date of the post',
+        example: '41234213412'
+    })
     @Expose()
     public timestamp: number;
 
+    @ApiProperty({
+        description: 'id of users',
+        example: '[loco, bank, bob, sam]'
+    })
     @Expose()
     public likes: string[];
 
+    @ApiProperty({
+        description: 'id of posts',
+        example: '[post1, post2, post3, post4]'
+    })
     @Expose()
     public comments: string[];
 }
