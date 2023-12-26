a2f9eeeda8f9d57c056e6fc56e4b856ca882f3c9
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-26 03:12:55 +0300

    fix(module2-task1): added get post

--- project/apps/blog/src/app/post/post.controller.ts
+++ project/apps/blog/src/app/post/post.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { PostService } from './post.service';
+import { fillDto } from '@project/shared/helpers';
+import { CreatePostDto } from './dto/create-post-dto';
+import { PostRdo } from './rdo/post.rdo';
+import { PostEntity } from './post.entity';
 
 @Controller('post')
 export class PostController {
     constructor(private readonly postService: PostService) {}
+
+    @Post('create-post')
+    public async create(@Body() dto: CreatePostDto) {
+        const newPost = await this.postService.createPost(dto);
+        return fillDto(PostRdo, newPost.toPOJO());
+    }
+
+    @Get(':id')
+    public async getPost(@Param('id') id: string) {
+        const existPost = await this.postService.getPost({ id }) as PostEntity;
+        return fillDto(PostRdo, existPost.toPOJO());
+    }
 }
