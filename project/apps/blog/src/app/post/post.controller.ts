2c53bced971db57eff2366e9b9a820bccd5e025d
Alexander <stdAlexTikhonov@hotmail.com>
2023-12-27 04:52:28 +0300

    fix(module2-task1): added endpoints

--- project/apps/blog/src/app/post/post.controller.ts
+++ project/apps/blog/src/app/post/post.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body, Get, Param, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, HttpStatus, Put } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PostService } from './post.service';
 import { fillDto } from '@project/shared/helpers';
 import { CreatePostDto } from './dto/create-post-dto';
 import { PostRdo } from './rdo/post.rdo';
 import { PostEntity } from './post.entity';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @ApiTags('blog')
 @Controller('post')
@@ -30,5 +31,27 @@ export class PostController {
     public async getPost(@Param('id') id: string) {
         const existPost = await this.postService.getPost({ id }) as PostEntity;
         return fillDto(PostRdo, existPost.toPOJO());
+    }
+
+    @ApiResponse({
+        type: PostRdo,
+        status: HttpStatus.OK,
+        description: 'Post was successfully updated.'
+    })
+    @Put(':id')
+    public async update(@Body() dto: UpdatePostDto) {
+        const newPost = await this.postService.updatePost(dto);
+        return fillDto(PostRdo, newPost.toPOJO());
+    }
+
+    @ApiResponse({
+        type: PostRdo,
+        status: HttpStatus.OK,
+        description: 'Posts found.'
+    })
+    @Get('all')
+    public async getPosts() {
+        const posts = await this.postService.getPosts() as PostEntity[];
+        return posts.map(item => fillDto(PostRdo, item.toPOJO()));
     }
 }
