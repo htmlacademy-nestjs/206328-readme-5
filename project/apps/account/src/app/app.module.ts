ef7fee8d7efd5529e8de292838c104198ed6332b
Alexander <stdAlexTikhonov@hotmail.com>
2024-01-10 09:35:07 +0300

    inject config account module

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ConfigAccountModule } from '@project/config/account';
 
 @Module({
-  imports: [UserModule, AuthenticationModule],
+  imports: [UserModule, AuthenticationModule, ConfigAccountModule],
   controllers: [],
   providers: [],
 })
