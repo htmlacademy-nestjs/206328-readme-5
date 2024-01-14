144b817a982dc988f672e2160b81c23ff99f8ecf
Alexander <stdAlexTikhonov@hotmail.com>
2024-01-14 13:19:00 +0300

    updated user repository

--- project/apps/account/src/app/user/user.repository.ts
+++ project/apps/account/src/app/user/user.repository.ts
@@ -1,12 +1,20 @@
-import { BaseMemoryRepository } from "@project/core";
-import { UserEntity } from "./user.entity";
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { BaseMongoRepository } from '@project/core';
+import { UserEntity } from './user.entity';
+import { BlogUserModel } from './blog-user.model';
 
 @Injectable()
-export class UserRepository extends BaseMemoryRepository<UserEntity> {
-    public findByEmail(email: string): Promise<UserEntity | null> {
-        const entities = Array.from(this.entities.values());
-        const user = entities.find((entity) => entity.email === email);
-        return Promise.resolve(user);
-    }
+export class UserRepository extends BaseMongoRepository<UserEntity, BlogUserModel> {
+  constructor(
+    @InjectModel(BlogUserModel.name) blogUserModel: Model<BlogUserModel>
+  ) {
+    super(blogUserModel, UserEntity.fromObject);
+  }
+  public async findByEmail(email: string): Promise<UserEntity | null> {
+    const document = await this.model.findOne({ email }).exec();
+    return this.createEntityFromDocument(document);
+  }
 }
