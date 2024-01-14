a2be57fee027c6a3a0edfe8629e6374960df7654
Alexander <stdAlexTikhonov@hotmail.com>
2024-01-14 12:48:57 +0300

    added mongoose module

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
-import { ConfigAccountModule } from '@project/config/account';
+import { ConfigAccountModule, getMongooseOptions } from '@project/config/account';
 
 @Module({
-  imports: [UserModule, AuthenticationModule, ConfigAccountModule],
+  imports: [
+    UserModule,
+    AuthenticationModule,
+    ConfigAccountModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
+  ],
   controllers: [],
   providers: [],
 })
