02dcca8f98c49640e87a6cea5f8b557ac7c4ac5e
Alexander <stdAlexTikhonov@hotmail.com>
2024-01-14 13:17:26 +0300

    updated user module

--- project/apps/account/src/app/user/user.module.ts
+++ project/apps/account/src/app/user/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { UserRepository } from './user.repository';
+import { BlogUserModel, BlogUserSchema } from './blog-user.model';
 
 @Module({
-    providers: [UserRepository],
-    exports: [UserRepository]
+  imports: [MongooseModule.forFeature([
+    { name: BlogUserModel.name, schema: BlogUserSchema }
+  ])],
+  providers: [UserRepository],
+  exports: [UserRepository],
 })
 export class UserModule {}
